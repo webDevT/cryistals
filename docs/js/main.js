6cfa57f1ca572e75fbde77b8e82daf30c437ea1c
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-12-27 22:26:55 +0300

    btn

--- docs/js/main.js
+++ docs/js/main.js
@@ -61,4 +61,54 @@ $(".lottery-tab").click(function() {
 
 
 });
+/*
+See https://greensock.com/draggable/ for details. 
+This demo uses InertiaPlugin which is a membership benefit of Club GreenSock, https://greensock.com/club/
+*/
+gsap.registerPlugin(InertiaPlugin);
 
+var $snap = $("#snap"),
+  $liveSnap = $("#liveSnap"),
+  $container = $("#container"),
+  gridWidth = $("body").width() / 5,
+  gridHeight = 100,
+  gridRows = 6,
+  gridColumns = 5,
+  i, x, y;
+
+//loop through and create the grid (a div for each cell). Feel free to tweak the variables above
+for (i = 0; i < gridRows * gridColumns; i++) {
+  y = Math.floor(i / gridColumns) * gridHeight;
+  x = (i * gridWidth) % (gridColumns * gridWidth);
+  $("<div/>").css({position:"absolute", width:gridWidth-1, height:gridHeight-1, top:y, left:x}).prependTo($container);
+}
+
+//set the container's size to match the grid, and ensure that the box widths/heights reflect the variables above
+gsap.set($container, {height: gridRows * gridHeight + 1, width: gridColumns * gridWidth + 1});
+gsap.set(".box", {width:gridWidth, height:gridHeight, lineHeight:gridHeight + "px"});
+gsap.set("#box2", {left: gridWidth * 2})
+
+//the update() function is what creates the Draggable according to the options selected (snapping).
+function update() {
+  var snap = $snap.prop("checked"),
+      liveSnap = $liveSnap.prop("checked");
+  Draggable.create(".box", {
+    bounds: $container,
+    edgeResistance: 0.65,
+    type: "x,y",
+    inertia: true,
+    autoScroll: true,
+    liveSnap: liveSnap,
+    snap:{
+      x: function(endValue) {
+        return (snap || liveSnap) ? Math.round(endValue / gridWidth) * gridWidth : endValue;
+      },
+      y: function(endValue) {
+        return (snap || liveSnap) ? Math.round(endValue / gridHeight) * gridHeight : endValue;
+      }
+    }
+  });
+}
+
+
+update();
